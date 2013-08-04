1ea5a5f083e6a8df97c6ce8fd07009780521b99b
ricardmo <marco.ricard@gmail.com>
2013-08-04 13:08:31 -0400

    Basic events integration

--- arab-phone.js
+++ arab-phone.js
@@ -1,24 +1,39 @@
 'use strict';
 
 var http = require('http'),
-    qs   = require('querystring');
+    qs   = require('querystring'),
+    Backbone = require('backbone'),
+    _ = require('underscore');
 
+
+var dataHandler = _.extend({}, Backbone.Events);
+
+dataHandler.listenTo( Backbone, 'translated', function( data ) {
+  console.log( data );
+});
+
+dataHandler.listenTo( Backbone, 'translate', function( phrase, from, to ) {
+  translate( phrase, from, to );
+});
 
 function languagePair ( from, to ) {
   return '' + from + '|' + to;
 }
 
-// request translation query
-var query = {
-  q: "this is a test phrase",
-  langpair: languagePair('en', 'fr')
-};
+function translate( phrase, from, to ) {
+  var query = {
+    q: phrase,
+    langpair: languagePair(from, to)
+  };
 
-var options = {
-  hostname: 'api.mymemory.translated.net',
-  path: 'get?' + qs.stringify( query )
-};
+  var options = {
+    hostname: 'api.mymemory.translated.net',
+    path: '/get?' + qs.stringify( query )
+  };
 
-http.get( options, function( res ) {
-  console.log( res );
-});
+  http.get( options, function( res ) {
+    res.on('data', function ( chunk ) { 
+      Backbone.trigger('translated', JSON.parse(chunk) );
+    });
+  });
+}
