0114148631170a6cd0be0a1814c1b18affca80c1
ricardmo <marco.ricard@gmail.com>
2013-08-04 19:05:18 -0400

    clean up

--- arab-phone.js
+++ arab-phone.js
@@ -6,80 +6,74 @@ var http = require('http'),
     _ = require('underscore');
 
 
+// Simple event handler to register asynchronous API callbacks
 var dataHandler = _.extend({}, Backbone.Events);
 
-var counter = 5;
-
-dataHandler.listenTo( Backbone, 'translated', function( data ) {
-  console.log( data );
-});
-
-dataHandler.listenTo( Backbone, 'translate', function( phrase, from, to ) {
-  translate( phrase, from, to );
-});
-
+// Returns a string for language pairs understood by the MyMemory API
 function languagePair ( from, to ) {
   return '' + from + '|' + to;
 }
 
-function languageChain () {
-  var languages = _.toArray( arguments ), pairs = [], i = 0;
-  while ( i < languages.length-1 ) pairs.push([ languages[i++], languages[i] ]);
+// Transforms a language chain to a list of language pairs.
+function languageChain ( languages ) {
+  var pairs = [], i = 0;
+  while ( i < languages.length-1 ) pairs.push({ from: languages[i], 
+                                                to:   languages[++i] });
   return pairs;
 }
 
+// Returns a normalized event name for translation 'from' language 'to' language.
 function eventName( from, to ) {
   return 'translated:' + from + ':' + to;
 }
 
+// Starts a translation chain, first argument is the phrase to translate, rest
+// is the language chain. ex: phone('this is funny', 'en', 'fr', 'es', 'ko', 'ru', 'en')
+// will translate english to french to spanish to korean to russian back to english. 
 function phone( phrase /*,languages*/) {
-  var pairs = languageChain.apply( this, [].splice.call(arguments, 1) ),
+  var pairs = languageChain( [].splice.call(arguments, 1) ),
       i = pairs.length-1;
 
   // Last event from the chain
-  dataHandler.listenToOnce( Backbone, eventName(pairs[i][0], pairs[i][1]), function(data) {
-    Backbone.trigger('translated', data.responseData.translatedText );
+  dataHandler.once( eventName(pairs[i].from, pairs[i].to), function(data) {
+    console.log( data.responseData.translatedText );
   });
 
   // Register events that trigger translation of the next sequence when we 
   // recieve data from the api.
-  while( i-- ) dataHandler.listenToOnce( Backbone, 
-                                         eventName(pairs[i][0], pairs[i][1]), 
-                                         next(pairs[i+1][0], pairs[i+1][1], i) );
-  // Start the chain
-  translate( phrase, pairs[0][0], pairs[0][1] );
+  while( i-- ) dataHandler.once( eventName(pairs[i].from, pairs[i].to), 
+                                 next(pairs[i+1].from, pairs[i+1].to, i) );
+  // Start the chain.
+  translate( phrase, pairs[0].from, pairs[0].to );
 }
 
 function next( from, to, i ) {
   return function( data ) {
     var phrase = data.responseData.translatedText;
-      console.log('translation step['+ i +']: ' + phrase );
-      console.log('translating "' + from + '" to "' + to + '"');
+      console.log('translation step['+ from +']: ' + phrase );
+      console.log('translating to "' + to + '"');
       translate( phrase, from, to );
   };
 }
 
+// Translates a phrase from language `from` to language `to`
 function translate( phrase, from, to ) {
-  var query = {
-    q: phrase,
-    langpair: languagePair(from, to)
-  };
-
-  var options = {
-    hostname: 'api.mymemory.translated.net',
-    path: '/get?' + qs.stringify( query )
-  };
+  var query = { q: phrase, langpair: languagePair(from, to), de: 'marco.ricard@gmail.com' },
+      options = {
+        hostname: 'api.mymemory.translated.net',
+        path: '/get?' + qs.stringify( query )
+      };
 
   http.get( options, function( res ) {
     var content = '';
     res.on('data', function ( chunk ) { content += chunk; });
-    res.on('end', function() { Backbone.trigger( eventName(from, to), 
-                                          JSON.parse(content) ); });
+    res.on('end', function() { dataHandler.trigger( eventName(from, to), 
+                                             JSON.parse(content) ); });
   });
 }
 
-phone('pendant que les jeunes vierges se baignent a la fontaine, les vieillards boivent et parient aux jeux de carte', 
-      'fr', 'en', 'de', 'fr', 'ru', 'es', 'fr', 'es', 'en', 'de', 'fr', 'ko', 'fr');
+phone('pendant que les jeunes vierges se baignent a la fontaine, les vieillards boivent et parient aux courses', 
+      'fr', 'en', 'de', 'fr', 'ru', 'es', 'fr', 'es', 'en', 'ko', 'fr');
 
 
 module.exports = {
